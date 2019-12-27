4961c3f29ca96f3f8869553e2221504bae89059b
J2TeamLBH <j2teamlbh@J2TeamLBHs-MacBook-Pro.local>
2019-12-27 17:18:59 +0700

    Updates

---
+++ precache-manifest.66ada07abb736d275c188b1b6a83d09b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add167ee82679eb98096bab594b9a2ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7b27d8fc3f0973071b",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "085ac54329a7f1334a1e",
+    "url": "/static/css/main.d04d1de7.chunk.css"
+  },
+  {
+    "revision": "7a7b27d8fc3f0973071b",
+    "url": "/static/js/2.ee8af325.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.ee8af325.chunk.js.LICENSE"
+  },
+  {
+    "revision": "085ac54329a7f1334a1e",
+    "url": "/static/js/main.6cd7c71c.chunk.js"
+  },
+  {
+    "revision": "556e05236b81b17f96b9",
+    "url": "/static/js/runtime-main.9081d04b.js"
+  }
+]);
